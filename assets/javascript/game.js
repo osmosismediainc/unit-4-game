a0458d955b9f14a8d105aaab52e122335385cd31
osmosismediainc <osmosismediainc@gmail.com>
2019-08-10 19:50:37 -0400

    Started with JavaScript functionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  let ranNum =  Math.floor(Math.random()*101+19) // Generates random number from 19 to 120
+  console.log("The Random Number: " + ranNum); //
+
+  let blueGem = Math.floor(Math.random()*12+1) // Generates random number 1-12
+  let redGem = Math.floor(Math.random()*12+1) // Generates random number 1-12
+  let greenGem = Math.floor(Math.random()*12+1) // Generates random number 1-12
+  let yellowGem = Math.floor(Math.random()*12+1) // Generates random number 1-12
+  console.log("blueGem: " + blueGem)
+  console.log("redGem: " + redGem)
+  console.log("greenGem: " + greenGem)
+  console.log("yellowGem: " + yellowGem)
+
+  
+  });
